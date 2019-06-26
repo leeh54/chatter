4b643a2b6c99338b8af77128e6127eff783d9665
Hyuk Gi Lee <hyukgilee@D-10-19-192-85.dhcp4.washington.edu>
2019-06-26 13:03:19 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Logo1.png';
+import TextInput from './TestInput';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="ABC">
+    <img src={logo} className="logo" alt="" />
+    chatApp
+    </header>
+    <TextInput/>
     </div>
   );
 }
---
+++ src/TestInput.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class TextInput extends React.Component {
+
+    render(){
+        return (<div className="text-input">
+        <input />
+        <button>SEND</button>
+        </div>)
+    }
+}
+
+export default TextInput
