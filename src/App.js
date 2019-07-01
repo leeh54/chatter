3a3b756f578e473e6af5a79ca819ff57079fff73
Hyuk Gi Lee <hyukgilee@Hyuks-MacBook-Pro.local>
2019-06-30 22:13:32 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from './Logo1.png';
+import logo from './logo5.png';
 import TextInput from './TestInput';
 import './App.css';
 function App() {
   return (
     <div className="App">
-    <header className="ABC">
-    <img src={logo} className="logo" alt="" />
-    chatApp
-    </header>
-    <TextInput/>
+      <header className="App-header">
+        Chats
+        <img src={logo} className="logo" alt="" />
+      </header>
+      <TextInput/>
     </div>
   );
 }
