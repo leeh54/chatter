85471142453da7e2b21d7a79bf7b0f9356ac80a6
Hyuk Gi Lee <hyukgilee@D-10-19-101-75.dhcp4.washington.edu>
2019-07-03 12:48:56 -0700

    updated again

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import React from 'react';
 import logo from './logo5.png';
 import TextInput from './TestInput';
-import TalkBubble from './TalkBubble';
 
 import './App.css';
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Chats
-        <img src={logo} className="logo" alt="" />
-      </header>
-      <TalkBubble/>
-      <TextInput/>
-    </div>
-  );
-}
+import NamePicker from './NamePicker';
 
+class App extends React.Component {
+  state = {
+    messages: [],
+    name: '',
+    editName: false,
+  }
+
+  sendMessage = (m) => {
+    var messages = [...this.state.messages, m]
+    this.setState({ messages })
+  }
+
+  render() {
+    /* console.log(this.state.messages); */
+    var { messages } = this.state
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="title-logo">
+            <img src={logo} className="logo" alt="" />
+            Chats
+        </div>
+          <NamePicker />
+        </header>
+        <main className="messages">
+          {
+            messages.map((m, i) => {
+              return <div key={i} className="bubble-wrap">
+                <div className="bubble">
+                  <span>{m}</span>
+                </div>
+              </div>
+            })
+          }
+        </main>
+        <TextInput sendMessage={this.sendMessage} />
+      </div>
+    );
+  }
+}
 export default App;
--- src/TestInput.js
+++ src/TestInput.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import { FiSend } from "react-icons/fi"; 
 
 class TextInput extends React.Component {
 
+    state={
+        text: "",
+        /*
+        text:"Write your message here...",
+        */
+    }
+
+    send = () => {
+        this.props.sendMessage(this.state.text)
+        this.setState({text:""})
+    }
+
+    keyPress = (e) => {
+        if(e.key === 'Enter'){
+            this.send()
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            text: e.target.value
+        })
+    }
+
     render(){
+        var {text} = this.state
         return (<div className="text-input">
-        <input />
-        <button>↑</button>
+        <input value={text}
+            onKeyPress={this.keyPress}
+            placeholder="Write your message here"
+            onChange={this.handleChange}
+        />
+        <button disabled={!text} onClick={this.send}>
+            <FiSend />
+        </button>
         </div>)
     }
 }
